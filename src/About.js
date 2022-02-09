e9f2465d1200be7ada4cfccc71a88388b147e975
somnath navale <somnathnavale112001@gmail.com>
2022-02-09 15:44:59 +0530

    about updated again

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <main className='About'>
       <h2>About</h2>
       <p>This Project is developed to improve understanding of ReactJs, React Router , asynchronous CRUD operations using APIs.</p>
-      <p>This is developed By <a href="https://github.com/somnathnavale" target='_blank' rel='author'> SOMNATH NAVALE </a></p>
+      <p>This is developed By <a href="https://github.com/somnathnavale" target='_blank' rel="noreferrer"> SOMNATH NAVALE </a></p>
     </main>
   );
 };
