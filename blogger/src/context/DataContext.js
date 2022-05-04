91f09352e3d2303382a1d1bca5eb7b8bb8d8959c
somnath navale <somnathnavale112001@gmail.com>
2022-05-04 10:36:17 +0530

    third commit

--- blogger/src/context/DataContext.js
+++ blogger/src/context/DataContext.js
@@ -1,6 +1,6 @@
 import { createContext, useState,useEffect } from "react";
-import useAxiosFetch from '../hooks/useAxiosFetch';
-
+// import useAxiosFetch from '../hooks/useAxiosFetch';
+import api from '../api/posts'
 const DataContext=createContext({});
 
 export const DataProvider=({children})=>{
@@ -8,11 +8,24 @@ export const DataProvider=({children})=>{
     const [posts,setPosts]=useState([]);
     const [search,setSearch]=useState('');
     const [searchResults,setSearchResults]=useState([]);
-    const {data,fetchError, isLoading}=useAxiosFetch('/posts') //localhost
+    const [fetchError,setFetchError]=useState(null);
+    const [isLoading ,setIsLoading]=useState(false);
+    // const {data,fetchError, isLoading}=useAxiosFetch('/posts') //localhost
 
     useEffect(()=>{
-        setPosts(data);
-    },[data])
+        const fetchPosts=async()=>{
+            try {
+                const response=await api.get('/posts');
+                console.log(response.data)
+                setPosts(response.data); 
+            } catch (error) {
+                setFetchError(error.message)
+            }finally{
+                isLoading(false);
+            }
+            fetchPosts();
+        }
+    },[])
 
     useEffect(()=>{
         const filteredResults=posts.filter(post=>
